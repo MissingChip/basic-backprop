6a2905b1601990a539d3a53f4d2e24d4a0842138
Ethan Ferguson <sometimescode@gmail.com>
2023-03-11 16:23:46 -0500

    Comments

--- learn.py
+++ learn.py
@@ -1,8 +1,11 @@
 
 import matplotlib.pyplot as plt
+import math
 import numpy as np
-import torch
 from tqdm import tqdm
+
+# using torch for utility
+from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 
 def downfrom(x, stop=-1, step=-1):
@@ -20,27 +23,34 @@ class Network():
         self.dparams = Network.new_params(shape)
         self.dactivations = Network.new_activations(shape, bias=False)
     
-    def forward_sample(self, sample):
+    def forward_sample(self, data):
         bias = 1.0
-        self.activations[0][:-1] = sample
+        # "0th" layer activation is the data
+        self.activations[0][:-1] = data
         self.activations[0][-1] = bias
         for L in range(1, len(self.activations)):
+            # calculate inputs from previous layer activations
             self.inputs[L][:] = self.params[L].dot(self.activations[L-1])
+            # calculate activations from inputs
             self.activations[L][:-1] = sigmoid(self.inputs[L])
+            # bias is implicit 
             self.activations[L][-1] = bias
         return self.activations[-1][:-1]
 
     def backprop(self, target):
         self.dactivations[-1] = dloss(self.activations[-1][:-1], target)
         for L in downfrom(len(self.params)-1, 0):
-            # print(self.dactivations[L].shape)
+            # gradient of inputs of layer
             dinputs = self.dactivations[L] * dsigmoid(self.inputs[L])
+            # gradient of parameters of layer
             dparams = np.outer(dinputs, self.activations[L-1])
+            # accumulate gradients
             self.dparams[L] -= dparams
-            # self.dactivations[L-1] = dinputs.dot(self.params[L])[:-1]
+            # gradient of previous layer
             self.dactivations[L-1] = self.params[L].transpose().dot(dinputs)[:-1]
     
     def update(self):
+        """Update parameters based on gradient"""
         for param, grad in zip(self.params[1:], self.dparams[1:]):
             param += self.lr*grad
             grad.fill(0)
@@ -56,7 +66,6 @@ class Network():
             target = np.zeros(10)
             target[y] = 1.0
             output = self.forward_sample(x)
-            # print(output, y)
             correct = (np.argmax(output) == y)
             avg_correct += correct
             avg_loss += loss(output, target)
@@ -72,7 +81,8 @@ class Network():
     def new_params(sizes):
         net = [None]
         for a, b in zip(sizes, sizes[1:]):
-            net.append(np.random.normal(size=(b, a+1)))
+            # Linear layer size a->b with bias
+            net.append(np.random.normal(size=(b, a+1), scale=1/math.sqrt(a)))
         return net
 
     @staticmethod
@@ -84,7 +94,6 @@ class Network():
 
 def loss(output, target):
     """loss function"""
-    # print(output, target)
     return np.mean((output-target)**2)
 
 def dloss(output, target):
@@ -92,26 +101,19 @@ def dloss(output, target):
     return 2*(output-target)
 
 def sigmoid(x):
+    """vectorized sigmoid function"""
     return 1/(1 + np.exp(-x))
 
 def dsigmoid(x):
+    """vectorized derivative of sigmoid"""
     o = sigmoid(x)
     return o*(1-o)
 
 def main():
     train_set = MNIST('data', train=True, download=True, transform=np.array)
     test_set = MNIST('data', train=False, transform=np.array)
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=32, shuffle=True)
-
-    if torch.cuda.is_available():
-        print("Cuda is available! (not in use)")
-    
-    image, label = test_set[10]
-    print(type(image), image.shape)
-    print(type(label), label)
-    # plt.imshow(image)
-    # plt.show()
+    train_loader = DataLoader(train_set, batch_size=32, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=32, shuffle=True)
 
     network_shape = [28*28, 16, 16, 10]
     net = Network(network_shape)
@@ -127,7 +129,7 @@ def main():
             avg_loss += loss
             avg_correct += correct
             i += 1
-            # net.lr = min(net.lr, loss/5, 0.1)*1.01
+        net.lr *= 0.1
         print(f"EPOCH {epoch} loss {avg_loss/i:.3f} correct {avg_correct/i*100:.1f}%")
     
     avg_correct = 0
